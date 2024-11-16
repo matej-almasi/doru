bbec2800910cb89ac1a3d52cdaac780d6773ebc3
Matej Almasi <almasi.mato@gmail.com>
2024-11-16 18:19:30 +0100

    refactor todo_manager and todo into separate modules

---
+++ src/todo.rs
@@ -0,0 +1,37 @@
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub enum TodoState {
+    Open,
+    InProgress,
+    Done,
+}
+#[derive(Debug, PartialEq)]
+pub struct Todo {
+    id: usize,
+    pub content: String,
+    pub state: TodoState,
+}
+
+impl Todo {
+    pub fn new(id: usize, content: &str) -> Self {
+        Self {
+            id,
+            content: String::from(content),
+            state: TodoState::Open,
+        }
+    }
+
+    pub fn get_id(&self) -> usize {
+        self.id
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn get_id() {
+        let todo = Todo::new(42, "Lorem Ipsum");
+        assert_eq!(todo.get_id(), 42);
+    }
+}
