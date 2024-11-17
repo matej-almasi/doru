96b89573a24549828dd11f98fa6ff47c37439e76
Matej Almasi <almasi.mato@gmail.com>
2024-11-17 12:00:49 +0100

    Implement Display for Todo

--- src/todo.rs
+++ src/todo.rs
@@ -1,9 +1,12 @@
+use std::fmt::Display;
+
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum TodoState {
     Open,
     InProgress,
     Done,
 }
+
 #[derive(Debug, PartialEq)]
 pub struct Todo {
     id: usize,
@@ -22,6 +25,22 @@ impl Todo {
 
     pub fn get_id(&self) -> usize {
         self.id
+    }
+}
+
+impl Display for Todo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let tick = if self.state == TodoState::Done {
+            "x"
+        } else {
+            " "
+        };
+
+        write!(
+            f,
+            "[{}] {:<20} [{:<12?}] (ID: {})",
+            tick, self.content, self.state, self.id
+        )
     }
 }
 
