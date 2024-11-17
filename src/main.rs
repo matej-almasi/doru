2743531ff95712e0bb13f298a8887dc850e75a7c
Matej Almasi <almasi.mato@gmail.com>
2024-11-17 12:01:39 +0100

    add basic clap arg parsing to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+use rudo::TodoManager;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { content: String },
+    // Edit { id: usize, content: String },
+    // List,
+    // Status { id: usize, status: String },
+    // Delete { id: usize },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let mut todo_manager = TodoManager::default();
+
+    match cli.command {
+        Commands::Add { content } => {
+            todo_manager.add_todo(&content);
+        }
+    }
+
+    // temporary: list all todos after command is executed
+    for todo in todo_manager.get_all() {
+        println!("{todo}");
+    }
 }
