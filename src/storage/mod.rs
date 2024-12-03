bc081af40eaf2357d3987fe4ca0ce1d1882af672
Matej Almasi <almasi.mato@gmail.com>
2024-12-03 18:39:30 +0100

    add documentation for mod storage

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,3 +1,9 @@
+//! File storage for `Todo`s.
+//!
+//! This module contains the [`TodoStorage`] trait that defines the contract for
+//! loading and storing `Todo`s from/ to arbitrary text format. An example
+//! [`JsonStorage`] type implementing the trait is provided.
+
 mod json_storage;
 mod todo_storage;
 
